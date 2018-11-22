5a98d7aa8bd6a84ec1791b0d61aff58f235ad29d
姬艳迪 <1002332733@qq.com>
2018-11-22 20:55:35 +0800

    二级分类功能完成

--- public/back/js/second.js
+++ public/back/js/second.js
@@ -4,6 +4,7 @@ $(function(){
   //1.当前页
   var currentPage = 1;
   var pageSize = 5;
+  var categoryId;
   //一进入页面 就渲染
   render();
   function render(){
@@ -41,9 +42,70 @@ $(function(){
     // 2.添加分类
     // 点击添加分类按钮  发送ajax
     $(".btn_add").click(function(){
-      $('#myModal').modal('show')
+      $('#myModal').modal('show');
+      // 请求ajax 数据  用来渲染二级分类下拉框
+      // 使用 page  pageSize 来模拟 
+      $.ajax({
+          url:"/category/queryTopCategoryPaging",
+          type:"get",
+          data:{
+            page:1,
+            pageSize:100,
+          },
+          dataType:"json",
+          success:function(info){
+                console.log(info);
+            var htmlStr = template("dropdowntpl",info);
+            $(".dropdown-menu").html(htmlStr);
+          }
+
+      })
+
     })
 
+    // 3. 给下拉菜单添加选中功能 (事件委托)
+     $(".dropdown-menu ").on("click","a",function(){
+          //获取a 的文本
+          var txt = $(this).text();
+          $(".btninfo").text(txt);
+          categoryId = $(this).data("id");
+          $('[name="categoryId"]').val(categoryId);
+     })
+ 
+    //4.图片上传  用插件
+    $('#fileupload').fileupload({
+      dataType: "json", // 返回回来的数据类型
+      done: function(e, data) {
+        console.log( data );
+        var result = data.result; // 后台返回的结果
+        // 获取图片地址, 赋值给 img 的 src
+        var picUrl = result.picAddr;
+        console.log(picUrl);
+        $(".imgbox img").attr("src", picUrl);
+        $('[name="brandLogo"]').val(picUrl)
+        
+        
+      }
+});
+   //5.表单内容上传
+  
+   
+   $(".btnAdd").click(function(){
+    // console.log($("#form").serialize());
+    $('#myModal').modal('hide');
+        $.ajax({
+          url:"/category/addSecondCategory",
+          type:"post",
+          dataType:"json",
+          data:$("#form").serialize(),
+          success:function(info){
+             console.log(info);
+
+             render();
+          }  
+        })
+        
+   })
     // //3.表单校验功能
     $("#form").bootstrapValidator({
     //       //1.指定图标  默认是bootstrap风格
@@ -68,7 +130,7 @@ $(function(){
     });
 
     // add-second-category 添加二级分类
-    
+
     //注册表单校验成功事件   阻止默认的表单提交  通过ajax提交
     $("#form").on("success.form.bv",function(e){   
     //       //阻止默认的提交

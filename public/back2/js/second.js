ff0e0aaa0d442dcf13bedf358f7f6e8094e64837
姬艳迪 <1002332733@qq.com>
2018-11-24 16:06:18 +0800

    二级分类分页完成

--- public/back2/js/second.js
+++ public/back2/js/second.js
@@ -17,6 +17,19 @@ $(function(){
                   console.log(info);
                     var str = template("secondtpl",info);
                     $('tbody').html(str);
+                    // 配置分页
+                    $(".pagintor").bootstrapPaginator({
+                      bootstrapMajorVersion:3,//默认是2，如果是bootstrap3版本，这个参数必填
+                      currentPage:info.page,//当前页
+                      totalPages:Math.ceil(info.total/info.size),//总页数
+                      // size:"small",//设置控件的大小，mini, small, normal,large
+                      onPageClicked:function(a, b, c,page){
+                        //为按钮绑定点击事件 page:当前点击的按钮值
+                        currentPage = page;
+                        render();
+                      }
+                    });
+                    
               }
           })
      }

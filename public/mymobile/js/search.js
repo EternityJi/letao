f51a506f0c3418f74b0b8e896b57e947d81e99db
姬艳迪 <1002332733@qq.com>
2018-11-25 19:40:15 +0800

    搜索页静态页面完成

---
+++ public/mymobile/js/search.js
@@ -0,0 +1,19 @@
+$(function(){
+  // 要渲染历史记录, 要先读取历史记录, 下面都是进行历史记录存取操作
+  // 我们需要约定一个键名, search_list
+
+  // 将来下面三句话, 可以放在控制台执行, 进行假数据初始化
+  // var arr = [ "耐克", "李宁", "新百伦", "耐克王", "阿迪王" ];
+  // var jsonStr = JSON.stringify( arr );
+  // localStorage.setItem( "search_list", jsonStr );
+
+
+  // 功能1: 历史记录渲染功能
+  // (1) 读取本地历史, 得到 jsonStr
+  // (2) 将 jsonStr 转换成 数组
+  // (3) 通过数组, 进行页面渲染(模板引擎)
+
+
+  // var arr = ["1","2","3","4"];
+  // var jsonStr = localStorage.getItem(JSON.stringify(str));
+})
